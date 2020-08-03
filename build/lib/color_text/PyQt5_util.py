abc093fa9c3032e5cfcae85a42c44e73f0fdf992
Qi Yixiang <Chong091010@163.com>
2020-08-03 22:41:47 +0800

    将 4.0.0 和其它版本分开了

---
+++ build/lib/color_text/PyQt5_util.py
@@ -0,0 +1,19 @@
+class ColorTextError(Exception):
+	pass
+
+try:
+	import PyQt5
+	del PyQt5
+
+except ModuleNotFoundError:
+	error_text = ['如果要使用此功能，请先在CMD（命令提示符）输入命令 “python -m pip install PyQt5 pyqt5-tools PyQt5-sip click python-dotenv” 以安装PyQt5、pyqt5-tools、PyQt5-sip、click和python-dotenv。',
+		'To use this feature, enter the command "Python-m pip install PyQt5 pyqt5-tools pyqt5-sip click python-dotenv" in CMD (command prompt) to install PyQt5, pyqt5-tools, PyQt5-sip, click, and python-dotenv.']
+	error = ColorTextError(error_text[1] + '\n' + '              ' + error_text[0])
+	raise error
+
+all_supported_controls = ['QCalendarWidget', 'QCheckBox', 'QComboBox', 'QCommandLinkButton', 'QDateEdit', 'QDateTimeEdit',
+	'QBoubleSpinBox', 'QFontComboBox', 'QKeySequenceEdit', 'QLabel', 'QLineEdit', 'QPlainTextEdit', 'QPushButton',
+	'QRadioButton', 'QSpinBox', 'QTextEdit', 'QTimeEdit', 'QDialogButtonBox']
+
+def SetControlTextColor(control, color):
+	control.setStyleSheet("* {color : " + str(color) + "}")
