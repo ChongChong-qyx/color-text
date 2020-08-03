38d4b986a510d3cebf160d759c5759c27e8fdf47
Qi Yixiang <Chong091010@163.com>
2020-08-03 22:36:02 +0800

    将 3.1.6 和其它版本分开了

---
+++ build/lib/color_text/__init__.py
@@ -0,0 +1,64 @@
+"""
+This is a library for printing colored texts.
+这是一个用来打印彩色文字的库。
+
+教程见 https://pypi.org/project/color-text/3.1.6/
+Tutorial see https://pypi.org/project/color-text/3.1.6/
+"""
+
+from platform import system
+import sys
+
+class _Version(str):
+	def __init__(self, version):
+		pass
+
+	def init_version_variable(self, number_1, number_2, number_3):
+		self.version_str = number_1 + '.' + number_2 + '.' + number_3
+		self.version_tuple = (int(number_1), int(number_2), int(number_3))
+		self.version_str_tuple = (number_1, number_2, number_3)
+		self.version_list = [int(number_1), int(number_2), int(number_3)]
+		self.version_str_list = [number_1, number_2, number_3]
+		self.version_dict = {'first_number' : int(number_1), 'second_number' : int(number_2), 'third_number' : int(number_3)}
+		self.version_str_dict = {'first_number' : number_1, 'second_number' : number_2, 'third_number' : number_3}
+
+if sys.version_info[:2] >= (3, 8):
+	import importlib.metadata as importlib_metadata
+else:
+	import importlib_metadata
+
+metadata = importlib_metadata.metadata("color_text")
+__title__ = metadata["name"]
+__summary__ = metadata["summary"]
+__version__ = metadata["version"]
+__author__ = metadata["author"]
+__email__ = metadata["author-email"]
+__license__ = metadata["license"]
+__copyright__ = "Copyright 2020 齐奕翔"
+__version__ = _Version(__version__)
+__version__.init_version_variable('3', '1', '6')
+
+del _Version
+del sys
+
+if system() == 'Windows':
+
+	from .output import PrintLightRedText, PrintYellowText, PrintRedText, PrintGreenText, PrintBlackText, PrintBlueText
+	from .output import PrintGreyText, PrintGrayText, PrintLightBlueText, PrintVeryLightGreenText, PrintLightPurpleText
+	from .output import PrintGlossWhiteText, PrintLightGreenText, output_example, Print, PrintOtherColorText, all_colors 
+	from .output import computer_language, PrintPurpleText, PrintWhiteText, PrintLightYellowText
+	from .get_input import GetBlackInput, GetBlueInput, GetGlossWhiteInput, GetGrayInput, GetGreenInput, GetGreyInput
+	from .get_input import GetLightBlueInput, GetLightGreenInput, GetLightPurpleInput, GetLightRedInput, GetLightYellowInput
+	from .get_input import GetPurpleInput, GetRedInput, GetVeryLightGreenInput, GetWhiteInput, GetYellowInput
+	from .get_input import GetOtherColorInput, GetInput
+	from .color import *
+	from .functions import *
+	from .handles import *
+
+else:
+
+	ErrorText = "This library is temporarily unavailable on the operating system you are using.\n         这个库暂时不能在你使用的操作系统上运行。"
+	Error = OSError(ErrorText)
+	raise Error
+	del Error
+	del ErrorText
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as file_object:
+	long_description = file_object.read()
+
+setuptools.setup(
+	name = "color_text",
+	version = "3.1.6",
+	author = "A Chinese working in FJA   一个在FJA工作的中国人",
+	author_email = "colortextpackage@yandex.com",
+	description = "A library for printing colored text.   一个用来打印彩色文字的库。",
+	long_description = long_description,
+	license = 'MIT',
+	long_description_content_type = "text/markdown",
+	url = None,
+	packages = ['color_text'],
+	zip_safe = False,
+	python_requires = '>=3.0, !=3.9.*, <4.0',
+	classifiers = [
+		"Programming Language :: Python :: 3.8",
+		"Operating System :: Microsoft :: Windows",
+		"License :: OSI Approved :: MIT License"
+	],
+	keywords = ["color_text", "color text", "colour_text", "colour text", "彩色文字", "有颜色的文字", "颜色文字",
+		"彩色文本", "有颜色的文本", "颜色文本", "color_words", "color words", "colour_words", "colour words", "color_word",
+		"color word", "colour_word", "colour word"],
+	platforms = ["Windows"],
+	scripts = ["color_text-example.cmd"]
+)
