6bc3d0317367a3d5fb2f3b3958e2459ee00ea7a2
Qi Yixiang <Chong091010@163.com>
2020-08-03 14:00:23 +0800

    将 1.1.0 和其它版本分开了

---
+++ color_text/tests/test_print_color.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-#
+
+from color_text import *
+
+Print("这是一段文字")
+
+printLightRed("这是一段浅红色的文字。")
+printYellow("这是一段黄色的文字。")
+printRed("这是一段红色的文字。")
+printGreen("这是一段绿色的文字。")
+printBlack("这是一段黑色的文字。")
+printBlue("这是一段蓝色的文字。")
+printPurple("这是一段紫色的文字。")
+printWhite("这是一段白色的文字。")
+printGrey("这是一段灰色的文字。")
+printGray("这也是一段灰色的文字。")
+printLightBlue("这是一段浅蓝色的文字。")
+printVeryLightGreen("这是一段很浅的绿色的文字。")
+printLightPurple("这是一段浅紫色的文字。")
+printLightYellow("这是一段浅黄色的文字。")
+printGlossWhite("这是一段亮白色的文字。")
+printLightGreen("这是一段浅绿色的文字。")
+printLightGreen("这也是一段浅绿色的文字。", 2)
+example()
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r",  encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="color_text",
+    version="1.1.0",
+    author="None",
+    author_email=None,
+    description="A library for printing colored text",
+    long_description=long_description,
+    license='MIT',
+    long_description_content_type="text/markdown",
+    url=None,
+    packages=['color_text', 'color_text/tests'],
+    zip_safe=False,
+    python_requires='==3.8.*',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
