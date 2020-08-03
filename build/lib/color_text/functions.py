ea8a4e1604f9fa600d6cb438a6a1c3f93b9962a0
Qi Yixiang <Chong091010@163.com>
2020-08-03 22:44:31 +0800

    将 4.1.0 和其它版本分开了

---
+++ build/lib/color_text/functions.py
@@ -0,0 +1,40 @@
+"""
+All functions for setting consoles text color.
+所有用来设置控制台的文本颜色的函数。
+"""
+
+from platform import system
+
+if system() == 'Windows':
+	from .handles import STD_OUTPUT_HANDLE, STD_INPUT_HANDLE, STD_ERROR_HANDLE
+	import ctypes
+	del system
+
+	std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+	std_error_handle = ctypes.windll.kernel32.GetStdHandle(STD_ERROR_HANDLE)
+	std_input_handle = ctypes.windll.kernel32.GetStdHandle(STD_INPUT_HANDLE)
+	
+	del ctypes
+	del STD_OUTPUT_HANDLE, STD_INPUT_HANDLE, STD_ERROR_HANDLE
+	
+	def set_cmd_text_color(color, handle = std_out_handle):
+		"""
+		Set the text color of consoles.
+		设置控制台的文本的颜色。
+		"""
+		import ctypes
+		Bool = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
+		del ctypes
+		return Bool
+	
+	def resetColor(handle = std_out_handle):
+		"""
+		Restore the text color of consoles to white.
+		将控制台的文本颜色恢复为白色。
+		"""
+		from .color import FOREGROUND_RED, FOREGROUND_GREEN, FOREGROUND_BLUE
+		set_cmd_text_color(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE, handle = handle)
+		del FOREGROUND_RED, FOREGROUND_GREEN, FOREGROUND_BLUE
+
+else:
+	del system
